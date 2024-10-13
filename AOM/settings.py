70d5b49ed5e8ec9133343d96b3eabd4c34092ac7
Jermaine_Riley Pasion <jmpasion@student.hau.edu.ph>
2024-10-13 22:50:44 +0800

    fix

--- AOM/settings.py
+++ AOM/settings.py
@@ -147,10 +147,3 @@ EMAIL_HOST_USER = 'angelite.online.marketplace@gmail.com'
 EMAIL_HOST_PASSWORD = 'xorepbenmeewdgfa'    
 APP_SCHEME = 'https'
 
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
