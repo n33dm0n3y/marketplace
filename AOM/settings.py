20515d125de39a7c57c938b996ad6b69b9905b3d
Jermaine_Riley Pasion <jmpasion@student.hau.edu.ph>
2024-10-13 18:31:45 +0800

    aws config

--- AOM/settings.py
+++ AOM/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'item',
     'dashboard',
     'inbox',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -145,3 +146,11 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'angelite.online.marketplace@gmail.com'
 EMAIL_HOST_PASSWORD = 'xorepbenmeewdgfa'    
 APP_SCHEME = 'https'
+
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
